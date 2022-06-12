8545b84bae7b157776da938de051dc90e9fb311a
VAMSHI CHOWDARY <89390696+Kollipati@users.noreply.github.com>
2022-06-12 09:35:44 +0530

    Add files via upload

---
+++ README.md.txt
@@ -0,0 +1,50 @@
+<h1>Flight-Fare-Prediction</h1>
+
+<h4>The aim of the project was to analyze the dataset and create an ML model that would predict the fare for the flight. We have used Python Libraries for data analysis and model creation (backend) and HTML and CSS for creating Web UI for the project.</h4>
+
+<h4>You can see and use our project live on : https://lnkd.in/gqyiFWY7</h4>
+<h4>To see the Demo and Explanation of the Project, visit : https://youtu.be/TnMNT8dyUZ8</h4>
+<br>
+
+<h1>Content</h1>
+<h4>
+1. Web Interface<br>
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp1.1 Landing Page<br> 
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp1.2 Predictor Page<br> 
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp1.3 About Us Page<br> 
+2. User Input<br>
+3. Result Page<br><br>
+</h4>
+
+<h2>1. Web Interface</h2>
+<h3>1.1 Landing Page</h3>
+<h4>When the User land on our webpage, he/she sees a webpage welcoming them to Flight Fare Prediction System and ask them to move further by clicking on the ‘Lets Go!’ button.</h4>
+<br>
+<img src = "https://github.com/RitikRatnawat/Flight-Fare-Prediction/blob/7a9f498abdcdedc7cfcc35a23644e7cf33ba26fd/static/images/Welcome%20Page.png">
+<br><br>
+
+<h3>1.2 Predictor Page</h3>
+<h4>This is the next page that can be accessed by clicking on ‘Lets Go!’ button on the homepage or by clicking on predictor tab in the navigation bar on the homepage
+The user sees various fields asking for information that is required to predict the price of a flight. Every user input has its own dropdown where the user can select their input.
+After providing the required input and pressing the submit button, the page refreshes and displays the predicted price of the flight.</h4>
+<br>
+<img src = "https://github.com/RitikRatnawat/Flight-Fare-Prediction/blob/90ba410ad7168da23b25c58fd8dd1b1e218ea24b/static/images/Predictor%20Page.png">
+<br><br>
+
+<h3>1.3 About Us Page</h3>
+<h4>The About us page holds a short summary about the people who have contributed in building this project. There are social links attached as well in case someone wants to contact the people behind this project.</h4>
+<br>
+<img src = "https://github.com/RitikRatnawat/Flight-Fare-Prediction/blob/d0badd766984d5569255712494f3dbfe278fd174/static/images/About%20Us%20Page.png">
+<br><br>
+  
+<h2>2. User Input</h2>
+<h4>On the predictor page, the user has to provide all the information asked for the prediction. The user can select from the drop down lists attached to each of the input fields. Once, all the asked information is provided, the user clicks on submit button to get the output.</h4>
+<br>
+<img src = "https://github.com/RitikRatnawat/Flight-Fare-Prediction/blob/4a87f498af2235372e455285c6dbb6c6947252f1/static/images/Predictor%20Page%20with%20Input.png">
+<br><br>
+
+<h2>3. Results Page</h2>
+<h4>On the predictor page, the user provides all the asked information and then clicks on submit button. The predicted fare of the selected flight is displayed to the user.</h4>
+<br>
+<img src = "https://github.com/RitikRatnawat/Flight-Fare-Prediction/blob/4a87f498af2235372e455285c6dbb6c6947252f1/static/images/Predictor%20Page%20with%20Output.png">
+<br><br>
---
+++ app.py
@@ -0,0 +1,327 @@
+from flask import Flask, render_template, request
+import sklearn
+import pickle
+import pandas as pd
+
+file = open(r"C:\Users\vamsh\Downloads\Flight-Fare-Prediction-master111\Flight-Fare-Prediction-master\app.pkl", "rb")
+model = pickle.load(file)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+        return render_template("welcome.html")
+
+@app.route("/predictor", methods = ["GET", "POST"])
+def predict():
+    
+    if request.method == "POST":
+        
+        # Departure Date
+        dep_t = request.form["Dep_Time"]
+        Journey_day = int(pd.to_datetime(dep_t, format = "%Y-%m-%dT%H:%M").day)
+        Journey_month = int(pd.to_datetime(dep_t, format = "%Y-%m-%dT%H:%M").month)
+        
+        # Departure Time
+        Dep_hour = int(pd.to_datetime(dep_t, format = "%Y-%m-%dT%H:%M").hour)
+        Dep_min = int(pd.to_datetime(dep_t, format = "%Y-%m-%dT%H:%M").minute)
+        
+        # Arrival Date
+        arr_t = request.form["Arr_Time"]
+        Arrival_hour = int(pd.to_datetime(arr_t, format = "%Y-%m-%dT%H:%M").hour)
+        Arrival_min = int(pd.to_datetime(arr_t, format = "%Y-%m-%dT%H:%M").minute)
+        
+        # Duration
+        Duration_min = abs(Arrival_hour - Dep_hour)*60 + abs(Arrival_min - Dep_min)
+        
+        # Total stops
+        Total_stops = int(request.form["Total_stops"])
+        
+        # Airline
+        airline = request.form["Airline"]
+        
+        if airline == "Jet Airways":
+            Jet_Airways = 1
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        elif airline == "IndiGo":
+            Jet_Airways = 0
+            IndiGo = 1
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        elif airline == "Air India":
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 1
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        elif airline == "Multiple carriers":
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 1
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        elif airline == "SpiceJet":
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 1
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        elif airline == "Vistara":
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 1
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        elif airline == "GoAir":
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 1
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        elif airline == "Multiple carriers Premium Economy":
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 1
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        elif airline == "Vistara Premium economy":
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 1
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        elif airline == "Jet Airways Business":
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 1
+            Trujet = 0
+            
+        elif airline == "Trujet":
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 1
+            
+        else:
+            Jet_Airways = 0
+            IndiGo = 0
+            Air_India = 0
+            Multiple_carriers = 0
+            SpiceJet = 0
+            Vistara = 0
+            Go_Air = 0 
+            Multiple_carriers_Premium_economy = 0
+            Vistara_Premium_economy = 0
+            Jet_Airways_Business = 0
+            Trujet = 0
+            
+        
+        # Source
+        source = request.form["Source"]
+        
+        if source == "Delhi":
+            s_Delhi = 1
+            s_Kolkata = 0
+            s_Mumbai = 0
+            s_Chennai = 0
+            
+        elif source == "Kolkata":
+            s_Delhi = 0
+            s_Kolkata = 1
+            s_Mumbai = 0
+            s_Chennai = 0
+            
+        elif source == "Mumbai":
+            s_Delhi = 0
+            s_Kolkata = 0
+            s_Mumbai = 1
+            s_Chennai = 0
+        
+        elif source == "Chennai":
+            s_Delhi = 0
+            s_Kolkata = 0
+            s_Mumbai = 0
+            s_Chennai = 1
+            
+        else:
+            s_Delhi = 0
+            s_Kolkata = 0
+            s_Mumbai = 0
+            s_Chennai = 0
+            
+        
+        # Destination
+        destination = request.form["Destination"]
+        
+        if destination == "Cochin":
+            d_Cochin = 1
+            d_Delhi = 0
+            d_New_Delhi = 0
+            d_Hyderabad = 0
+            d_Kolkata = 0
+            
+        elif destination == "Delhi":
+            d_Cochin = 0
+            d_Delhi = 1
+            d_New_Delhi = 0
+            d_Hyderabad = 0
+            d_Kolkata = 0
+            
+        if destination == "New Delhi":
+            d_Cochin = 0
+            d_Delhi = 0
+            d_New_Delhi = 1
+            d_Hyderabad = 0
+            d_Kolkata = 0
+            
+        if destination == "Hyderabad":
+            d_Cochin = 0
+            d_Delhi = 0
+            d_New_Delhi = 0
+            d_Hyderabad = 1
+            d_Kolkata = 0
+            
+        if destination == "Kolkata":
+            d_Cochin = 0
+            d_Delhi = 0
+            d_New_Delhi = 0
+            d_Hyderabad = 0
+            d_Kolkata = 1
+            
+        else:
+            d_Cochin = 0
+            d_Delhi = 0
+            d_New_Delhi = 0
+            d_Hyderabad = 0
+            d_Kolkata = 0
+            
+            
+        prediction = model.predict([[
+            Total_stops,
+            Journey_day,
+            Journey_month,
+            Arrival_hour,
+            Arrival_min,
+            Dep_hour,
+            Dep_min,
+            Duration_min,
+            s_Chennai,
+            s_Delhi,
+            s_Kolkata,
+            s_Mumbai,
+            d_Cochin,
+            d_Delhi,
+            d_Hyderabad,
+            d_Kolkata,
+            d_New_Delhi,
+            Air_India,
+            Go_Air,
+            IndiGo,
+            Jet_Airways,
+            Jet_Airways_Business,
+            Multiple_carriers,
+            Multiple_carriers_Premium_economy,
+            SpiceJet,
+            Trujet,
+            Vistara,
+            Vistara_Premium_economy
+        ]])
+        
+        output = round(prediction[0], 2)
+        print(output)
+        
+        return render_template("predictor.html", 
+                               prediction_text = "Your Flight Fare from {} to {} is Rs. {}".format(source, destination, output))
+                   
+        
+    return render_template("predictor.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("aboutus.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
